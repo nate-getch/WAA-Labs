ce50d942942a5b8050ea5d9d96d969d8cab93c64
985931@CS.MUM.EDU <985931@CS.MUM.EDU>
2017-10-11 14:32:10 -0600

    added resource dir @lab6

--- Lab7_Valid/src/main/java/com/students/contoller/HomeController.java
+++ Lab7_Valid/src/main/java/com/students/contoller/HomeController.java
@@ -17,7 +17,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
  
-	@RequestMapping(value = "/",method=RequestMethod.GET)
+	//@RequestMapping(value = "/",method=RequestMethod.GET)
+	@RequestMapping
 	public String home( Model model) {
  		
  		return "home";
